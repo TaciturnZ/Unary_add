477309a4e452417777f6fceabb7b898fb3df3bf9
TaciturnZ <2123748691@qq.com>
2024-11-07 11:36:59 +0800

    first

---
+++ Unary_add_1_4_3/Unary_add_1_4_3.v
@@ -0,0 +1,49 @@
+module Unary_add_1_4_3 (
+    input wire A,
+    input wire B,
+    input wire en,
+    input wire clk,
+    input wire rst_n,
+    input wire read_or_write,
+    output reg dout,
+    output reg C
+);
+
+    reg [1:0] count;        
+    reg flag;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            count <= 2'd0;
+            dout <= 1'b0;
+            C <= 1'b0;
+            flag <= 1'b0;
+        end else if (en) begin
+            if (read_or_write == 1'b0) begin
+                // 读取阶段
+                dout <= 1'b0;
+                C <= 1'b0;
+                if (((count == 2'd3) && (A || B)) || ((count == 2'd2) && (A && B))) begin
+                    flag <= 1'b1;
+                end
+                if (A && B) begin
+                    count <= count + 2;
+                end else if (A || B) begin
+                    count <= count + 1;
+                end if (flag == 1'b1) begin
+                    C <= 1'b1;
+                    flag <= 1'b0;
+                end
+            end else begin
+                // 写入阶段
+                C <= 1'b0;
+                if (count) begin
+                    dout <= 1'b1;
+                    count <= count - 1; 
+                end else
+                    dout <= 1'b0;
+                end 
+            end
+        end
+
+endmodule
